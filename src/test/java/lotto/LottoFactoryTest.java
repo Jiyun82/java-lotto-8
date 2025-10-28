3e27dfa30717d539aaffcbfd0d55ad9ece19716e
Jiyun82 <202110918@sangmyung.kr>
2025-10-28 17:51:47 +0900

    test: LottoFactoryTest에 중복, 번호 갯수, 범위 검사 단위 테스트 추가

--- src/test/java/lotto/LottoFactoryTest.java
+++ src/test/java/lotto/LottoFactoryTest.java
@@ -2,13 +2,17 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.domain.Lotto;
 import lotto.domain.LottoFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoFactoryTest {
+
+    private final int LOTTO_NUMBER_COUNT = 6;
 
     @Test
     @DisplayName("주어진 갯수만큼 로또를 생성한다.")
@@ -19,6 +23,38 @@ public class LottoFactoryTest {
         List<Lotto> lottos = lottoFactory.createLottos(5);
 
         assertThat(lottos).hasSize(count);
+    }
+
+    @Test
+    @DisplayName("로또의 번호는 중복 없이 6개여야 한다.")
+    void shouldHaveSixUniqueNumbers() {
+        int count = 1;
+
+        LottoFactory lottoFactory = new LottoFactory();
+        List<Lotto> lottos = lottoFactory.createLottos(count);
+
+        for (Lotto lotto : lottos) {
+            List<Integer> numbers = lotto.getNumbers();
+            Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+            assertThat(numbers).hasSize(LOTTO_NUMBER_COUNT);
+            assertThat(uniqueNumbers).hasSize(LOTTO_NUMBER_COUNT);
+        }
+    }
+
+    @Test
+    @DisplayName("로또의 번호는 1~45 범위를 벗어나지 않아야 한다.")
+    void shouldHaveNumbersWithinRange() {
+        int count = 1;
+
+        LottoFactory lottoFactory = new LottoFactory();
+        List<Lotto> lottos = lottoFactory.createLottos(count);
+
+        for (Lotto lotto : lottos) {
+            List<Integer> numbers = lotto.getNumbers();
+
+            assertThat(numbers).allMatch(n -> n >= 1 && n <= 45);
+        }
     }
 
 }
