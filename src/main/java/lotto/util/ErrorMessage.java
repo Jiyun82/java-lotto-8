d8f3e6f87ebb9ace7161f136f74f617b6ffb03f3
Jiyun82 <202110918@sangmyung.kr>
2025-10-28 17:10:34 +0900

    feat: 예외 메시지 상수 클래스(ErrorMessage) 추가

---
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public class ErrorMessage {
+
+    public static final String NOT_POSITIVE_INTEGER = "양의 정수를 입력해주세요.";
+
+    public static final String NOT_MULTIPLE_OF_THOUSAND = "1000 단위로 입력해주세요.";
+
+}
