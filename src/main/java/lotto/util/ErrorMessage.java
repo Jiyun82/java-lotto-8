5b7d90ad5dce29fbe70195dea962ad3db364f949
Jiyun82 <202110918@sangmyung.kr>
2025-10-31 19:54:03 +0900

    fix: 에러 메시지 앞에 [ERROR] 문구 추가

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -2,10 +2,12 @@ package lotto.util;
 
 public class ErrorMessage {
 
-    public static final String NOT_POSITIVE_INTEGER = "양의 정수를 입력해주세요.";
+    public static final String NOT_POSITIVE_INTEGER = "[ERROR] 양의 정수를 입력해주세요.";
 
-    public static final String NOT_MULTIPLE_OF_THOUSAND = "1000 단위로 입력해주세요.";
+    public static final String NOT_MULTIPLE_OF_THOUSAND = "[ERROR] 1000 단위로 입력해주세요.";
 
-    public static final String NOT_INTEGER = "정수를 입력해주세요.";
+    public static final String NOT_INTEGER = "[ERROR] 정수를 입력해주세요.";
+
+    public static final String NOT_SIX_LOTTO_NUMBERS = "[ERROR] 로또 번호는 6개여야 합니다.";
 
 }
