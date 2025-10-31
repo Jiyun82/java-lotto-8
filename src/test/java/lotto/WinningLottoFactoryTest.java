6ead3da314ed2692b0096c94234cfde509435c64
Jiyun82 <202110918@sangmyung.kr>
2025-10-31 22:17:55 +0900

    test: 불변 리스트를 가변 리스트로 수정

--- src/test/java/lotto/WinningLottoFactoryTest.java
+++ src/test/java/lotto/WinningLottoFactoryTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoFactory;
@@ -19,7 +20,7 @@ public class WinningLottoFactoryTest {
     @Test
     @DisplayName("당첨 번호와 보너스 번호가 주어지면 WinningLotto 객체를 생성한다.")
     void createWinningLotto_shouldCreateWinningLottoWhenValidInput() {
-        List<Integer> winningNumbers = List.of(2, 10, 16, 22, 30, 43);
+        List<Integer> winningNumbers = new ArrayList<>(List.of(2, 10, 16, 22, 30, 43));
         int bonusNumber = 7;
 
         LottoFactory lottoFactory = new LottoFactory();
@@ -37,7 +38,7 @@ public class WinningLottoFactoryTest {
     @ValueSource(ints = {-10, 0, 46})
     @DisplayName("보너스 번호의 범위가 1~45를 벗어날 경우 예외를 던진다.")
     void createWinningLotto_shouldThrowExceptionWhenBonusNumberOutOfRange(int bonusNumber) {
-        List<Integer> winningNumbers = List.of(2, 10, 16, 22, 30, 43);
+        List<Integer> winningNumbers = new ArrayList<>(List.of(2, 10, 16, 22, 30, 43));
 
         LottoFactory lottoFactory = new LottoFactory();
         Lotto lotto = lottoFactory.createLotto(winningNumbers);
