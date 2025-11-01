1fcebfc3020c878fc965ab51fa227954438ea2ca
Jiyun82 <202110918@sangmyung.kr>
2025-11-01 23:15:02 +0900

    test: WinningCheckerTest 단위 테스트 수정

--- src/test/java/lotto/WinningCheckerTest.java
+++ src/test/java/lotto/WinningCheckerTest.java
@@ -1,12 +1,16 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+import lotto.domain.Lotto;
 import lotto.domain.WinningChecker;
+import lotto.domain.WinningLotto;
+import lotto.domain.WinningType;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,42 +18,41 @@ import org.junit.jupiter.params.provider.MethodSource;
 public class WinningCheckerTest {
 
     @ParameterizedTest
-    @MethodSource("provideWinningNumbers")
-    @DisplayName("로또 번호와 당첨 번호의 일치 개수를 반환한다.")
-    void countMatchingNumbers_shouldReturnCorrectCount(List<Integer> numbers, List<Integer> winningNumbers, int expected) {
+    @MethodSource("winningTypeProvider")
+    @DisplayName("로또 번호와 당첨 번호 및 보너스 번호를 비교하여 당첨 결과를 반환한다.")
+    void calculateWinningType_returnsWinningResult(Lotto lotto, WinningLotto winningLotto, WinningType expectedType) {
         WinningChecker winningChecker = new WinningChecker();
 
-        int matchCount = winningChecker.countMatchingNumbers(numbers, winningNumbers);
-
-        assertThat(matchCount).isEqualTo(expected);
+        assertThat(winningChecker.calculateWinningType(lotto, winningLotto)).isEqualTo(expectedType);
     }
 
-    @Test
-    @DisplayName("로또 번호에 보너스 번호가 포함되어 있다면 true를 반환한다.")
-    void isBonusMatched_shouldReturnTrueWhenBonusIsIncluded() {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 2;
-
-        WinningChecker winningChecker = new WinningChecker();
-
-        assertThat(winningChecker.isBonusMatched(numbers, bonusNumber)).isTrue();
-    }
-
-    @Test
-    @DisplayName("로또 번호에 보너스 번호가 포함되어 있지 않다면 false를 반환한다.")
-    void isBonusMatched_shouldReturnFalseWhenBonusIsNotIncluded() {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 7;
-
-        WinningChecker winningChecker = new WinningChecker();
-
-        assertThat(winningChecker.isBonusMatched(numbers, bonusNumber)).isFalse();
-    }
-
-    static Stream<Arguments> provideWinningNumbers() {
+    static Stream<Arguments> winningTypeProvider() {
         return Stream.of(
-                Arguments.of(List.of(1,2,3,4,5,6), List.of(1,2,3,4,5,6), 6),
-                Arguments.of(List.of(1,2,3,4,5,6), List.of(1,2,3,7,8,9), 3)
+                arguments(
+                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 7, 8, 9))),
+                        new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 10),
+                        WinningType.THREE_MATCH
+                ),
+                arguments(
+                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 8, 9))),
+                        new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 10),
+                        WinningType.FOUR_MATCH
+                ),
+                arguments(
+                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 9))),
+                        new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 10),
+                        WinningType.FIVE_MATCH
+                ),
+                arguments(
+                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 10))),
+                        new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 10),
+                        WinningType.FIVE_BONUS
+                ),
+                arguments(
+                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6))),
+                        new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 10),
+                        WinningType.SIX_MATCH
+                )
         );
     }
 
