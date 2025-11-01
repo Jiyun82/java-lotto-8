a99623d5fa3e0a6eda9f6eb14c603e2cedc7dbde
Jiyun82 <202110918@sangmyung.kr>
2025-11-01 23:26:18 +0900

    feat: 당첨 결과 ENUM 생성

---
+++ src/main/java/lotto/domain/WinningType.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+public enum WinningType {
+
+    THREE_MATCH(3, false, 5000),
+    FOUR_MATCH(4, false, 50000),
+    FIVE_MATCH(5, false, 1500000),
+    FIVE_BONUS(5, true, 30000000),
+    SIX_MATCH(6, false, 2000000000);
+
+    private final int matchCount;
+
+    private final boolean bonusMatched;
+
+    private final int prize;
+
+    WinningType(int matchCount, boolean bonusMatched, int prize) {
+        this.matchCount = matchCount;
+        this.bonusMatched = bonusMatched;
+        this.prize = prize;
+    }
+
+    public String toString() {
+        if(bonusMatched) {
+            return matchCount + "개 일치, 보너스 볼 일치 (" + String.format("%,d", prize) + "원) - ";
+        }
+        return matchCount + "개 일치 (" + String.format("%,d", prize) + "원) - ";
+    }
+
+}
