e271d9af182c9ac7d59b9fcd62db4dc8d7a792a4
Jiyun82 <202110918@sangmyung.kr>
2025-11-03 00:06:04 +0900

    test: WinningChecker 단위 테스트 수정

--- src/test/java/lotto/WinningCheckerTest.java
+++ src/test/java/lotto/WinningCheckerTest.java
@@ -1,68 +1,40 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 import lotto.domain.Lotto;
 import lotto.domain.WinningChecker;
 import lotto.domain.WinningLotto;
+import lotto.domain.WinningResults;
 import lotto.domain.WinningType;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 public class WinningCheckerTest {
 
-    @ParameterizedTest
-    @MethodSource("winningTypeProvider")
+    @Test
     @DisplayName("로또 번호와 당첨 번호 및 보너스 번호를 비교하여 당첨 결과를 반환한다.")
-    void calculateWinningType_returnsWinningResult(Lotto lotto, WinningLotto winningLotto, WinningType expectedType) {
-        WinningChecker winningChecker = new WinningChecker();
+    void calculateWinningResults_shouldReturnWinningResultsWhenLottosAndWinningLottoGiven() {
+        Lotto lotto1 = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+        Lotto lotto2 = new Lotto(new ArrayList<>(List.of(7, 8, 9, 10, 11, 12)));
+        Lotto lotto3 = new Lotto(new ArrayList<>(List.of(13, 14, 15, 16, 16, 18)));
+        Lotto lotto4 = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 7)));
+        List<Lotto> lottos = List.of(lotto1, lotto2, lotto3, lotto4);
 
-        assertThat(winningChecker.calculateWinningType(lotto, winningLotto)).isEqualTo(expectedType);
-    }
-
-    static Stream<Arguments> winningTypeProvider() {
-        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+        Lotto winningNumbers = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
         int bonusNumber = 7;
-        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
 
-        return Stream.of(
-                arguments(
-                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 7, 8, 9))),
-                        winningLotto,
-                        WinningType.THREE_MATCH
-                ),
-                arguments(
-                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 8, 9))),
-                        winningLotto,
-                        WinningType.FOUR_MATCH
-                ),
-                arguments(
-                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 9))),
-                        winningLotto,
-                        WinningType.FIVE_MATCH
-                ),
-                arguments(
-                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 7))),
-                        winningLotto,
-                        WinningType.FIVE_BONUS
-                ),
-                arguments(
-                        new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6))),
-                        winningLotto,
-                        WinningType.SIX_MATCH
-                ),
-                arguments(
-                        new Lotto(new ArrayList<>(List.of(10, 11, 12, 13, 14, 15))),
-                        winningLotto,
-                        null
-                )
-        );
+        WinningChecker winningChecker = new WinningChecker();
+        WinningResults results = winningChecker.calculateWinningResults(lottos, winningLotto);
+
+        assertThat(results.getCountByWinningType(WinningType.THREE_MATCH)).isEqualTo(0);
+        assertThat(results.getCountByWinningType(WinningType.FOUR_MATCH)).isEqualTo(0);
+        assertThat(results.getCountByWinningType(WinningType.FIVE_MATCH)).isEqualTo(0);
+        assertThat(results.getCountByWinningType(WinningType.FIVE_BONUS)).isEqualTo(1);
+        assertThat(results.getCountByWinningType(WinningType.SIX_MATCH)).isEqualTo(1);
     }
 
 }
