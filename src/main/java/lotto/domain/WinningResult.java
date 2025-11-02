6a5a8ea773f2d22d18fea54f0096709c8ddf3236
Jiyun82 <202110918@sangmyung.kr>
2025-11-02 23:16:10 +0900

    feat: 당첨 결과를 저장하는 WinningResults 및 WinningResult 구현

---
+++ src/main/java/lotto/domain/WinningResult.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public class WinningResult {
+
+    private final WinningType winningType;
+
+    private int count;
+
+    public WinningResult(WinningType winningType) {
+        this.winningType = winningType;
+        this.count = 0;
+    }
+
+    public void increaseCount() {
+        count++;
+    }
+
+    public WinningType getWinningType() {
+        return winningType;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ src/main/java/lotto/domain/WinningResults.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningResults {
+
+    private final List<WinningResult> results;
+
+    public WinningResults() {
+        results = new ArrayList<>();
+        for (WinningType type : WinningType.values()) {
+            results.add(new WinningResult(type));
+        }
+    }
+
+    public void addResult(WinningType winningType) {
+        results.stream()
+                .filter(result -> result.getWinningType() == winningType)
+                .forEach(WinningResult::increaseCount);
+    }
+
+    public List<WinningType> getWinningTypes() {
+        List<WinningType> winningTypes = new ArrayList<>();
+        for (WinningResult result : results) {
+            winningTypes.add(result.getWinningType());
+        }
+        return winningTypes;
+    }
+
+    public List<Integer> getCounts() {
+        List<Integer> counts = new ArrayList<>();
+        for (WinningResult result : results) {
+            counts.add(result.getCount());
+        }
+        return counts;
+    }
+
+    public Integer getCountByWinningType(WinningType winningType) {
+        return results.stream()
+                .filter(result -> result.getWinningType() == winningType)
+                .map(WinningResult::getCount)
+                .findFirst()
+                .orElse(null);
+    }
+
+}
