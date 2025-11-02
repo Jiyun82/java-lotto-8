bdd81d83d5f94ed8a6c056935662c4e2529265bd
Jiyun82 <202110918@sangmyung.kr>
2025-11-03 00:13:41 +0900

    feat: 최종 당첨 결과 계산 로직 추가

--- src/main/java/lotto/domain/WinningChecker.java
+++ src/main/java/lotto/domain/WinningChecker.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 public class WinningChecker {
 
-    public WinningType calculateWinningType(Lotto lotto, WinningLotto winningLotto) {
+    public WinningResults calculateWinningResults(List<Lotto> lottos, WinningLotto winningLotto) {
+        WinningResults results = new WinningResults();
+        for (Lotto lotto : lottos) {
+            WinningType winningType = calculateWinningType(lotto, winningLotto);
+            results.addResult(winningType);
+        }
+        return results;
+    }
+
+    private WinningType calculateWinningType(Lotto lotto, WinningLotto winningLotto) {
         int matchCount = countMatchingNumbers(lotto.getNumbers(), winningLotto.getWinningNumbers());
         boolean bonusMatched = isBonusMatched(lotto.getNumbers(), winningLotto.getBonusNumber());
 
