2e9b11160fb36e2c9d19c585de11b5095cf9b763
Jiyun82 <202110918@sangmyung.kr>
2025-11-03 14:15:11 +0900

    test: LottoService 단위 테스트 추가(양수, 1000 단위 검증)

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -1,13 +1,17 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.WinningLotto;
 import lotto.service.LottoService;
+import lotto.util.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoServiceTest {
 
@@ -21,6 +25,29 @@ public class LottoServiceTest {
         int lottoPrice = 1000;
         int count = purchaseAmount / lottoPrice;
         assertThat(lottoService.purchaseLottos(purchaseAmount)).hasSize(count);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1000})
+    @DisplayName("구입 금액이 양수가 아닐 경우 예외를 던진다.")
+    void purchaseLottos_shouldThrowExceptionWhenPurchaseAmountIsNotPositive(int purchaseAmount) {
+        LottoService lottoService = new LottoService();
+
+        assertThatThrownBy(() -> lottoService.purchaseLottos(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.NOT_POSITIVE_INTEGER);
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1000 단위가 아닐 경우 예외를 던진다.")
+    void purchaseLottos_shouldThrowExceptionWhenPurchaseAmountIsNotMultipleOfThousand(){
+        int purchaseAmount = 10;
+
+        LottoService lottoService = new LottoService();
+
+        assertThatThrownBy(() -> lottoService.purchaseLottos(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.NOT_MULTIPLE_OF_THOUSAND);
     }
 
     @Test
